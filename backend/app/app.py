a62acb2ce60602f446629c3df9c388e2e3c47bd0
Lei Chen <mashirocl@gmail.com>
2025-02-24 21:18:07 +0900

    feat: file upload

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,12 +1,30 @@
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
+import os
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = "data"
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
 
 
 @app.route("/api/hello", methods=["GET"])
 def hello():
-    return jsonify({"message":"Hello pdfquery"}), 200
+    return jsonify({"message": "Hello pdfquery"}), 200
+
+
+@app.route("api/upload", methods=["POST"])
+def upload():
+    if "file" not in request.files:
+        return jsonify({"error": "No file uploaded"}), 400
+
+    file = request.files["file"]
+    if file.filename == "":
+        return jsonify({"error": "File not selected"}), 400
+
+    if file and file.filename.endswith(".pdf"):
+        file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+        file.save(file_path)
 
 
 if __name__ == "__main__":
